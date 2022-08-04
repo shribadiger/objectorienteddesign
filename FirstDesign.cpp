7a2f52fcc915ed5f361c8d047b08a1450f7e2a57
Shrikant Badiger <shrikant200@gmail.com>
2022-08-04 21:48:40 +0530

    Update FirstDesign.cpp

--- FirstDesign.cpp
+++ FirstDesign.cpp
@@ -1,1 +1,18 @@
 /* First design code having the multiple classes*/
+#include<iostream>
+using namespace std;
+
+class DefintionPage  {
+      private:
+          int _definitionID;
+          int _undefinetedID;
+          bool _isControllerInput;
+          int _maxRetract;
+  
+      public:
+         int getDefinitionID() { return _definitionUD; }
+         int getUndefinetedID() { return _undefinedID; }
+         bool getControllerInputFlag() { return _isControlledInput; } 
+         int getMaxReact() { return _maxRetract; }
+};
+
